973cae6b2f6b1d0f8b34621612155c0ac0d06916
SahasTech22 <sahastech22@gmail.com>
2024-10-29 17:28:39 +0530

    Update movie.js

--- Plugin/movie.js
+++ Plugin/movie.js
@@ -5,7 +5,7 @@ const config = require('../config'); // Ensure your API key is in config
 cmd({
     pattern: "movie",
     desc: "Fetch detailed information about a movie.",
-    category: "other",
+    category: "movie",
     react: "🎬",
     filename: __filename
 },
