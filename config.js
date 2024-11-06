b8bcb96a3004e5c567f7855ec7d55a587bb2a404
Sahas Tech <sahastech22@gmail.com>
2024-11-06 12:55:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "4mECEYrK#k1V3iB2LCGikEV3O7d5eCAvkUT2oM5Vi9ed29ZU0clU",
+  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
