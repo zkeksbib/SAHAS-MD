f7bc0c6bfe0ed727e79520a0c40aa532d3a0d042
zkeksbib <140790037+zkeksbib@users.noreply.github.com>
2024-11-09 23:30:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "Put Session Id",
+  SESSION_ID: process.env.SESSION_ID || "ITtk1aQZ#tWmh4UCzOTWHa1iKt6Qj0upZanjxqLGIr1cwAEETo3o",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
